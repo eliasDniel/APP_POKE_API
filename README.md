b2121a16b4812014b2d4e22d14fac9f0050a3bd2
eliasDniel <eramosg2@unemi.edu.ec>
2025-08-24 12:54:40 -0500

    inicio de la app

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_poke_api
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_poke_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_poke_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
